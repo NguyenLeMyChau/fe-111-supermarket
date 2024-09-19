33f24f2a8a61c5032fc490c61195e5b3a7a19139
Hoang <hoangghcc@gmail.com>
2024-09-19 22:11:12 +0700

    router staff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Frame from './containers/frame/Frame.js';
+import Frame_Staff from './containers/frame_Staff/Frame_Staff.js';
 import Login from './pages/login/Login.js';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/frame" element={<Frame />} />
+        <Route path="/frame_staff" element={<Frame_Staff/>} />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
