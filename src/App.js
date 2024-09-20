226ff7a18a3ea1fae9ac0f9b833c35cb08b507b8
My Chau <nguyenlemychau1201@gmail.com>
2024-09-20 20:46:21 +0700

    fix name frame staff in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Frame from './containers/frame/Frame.js';
-import Frame_Staff from './containers/frame_Staff/Frame_Staff.js';
+import FrameStaff from './containers/frame_Staff/Frame_Staff.js';
 import Login from './pages/login/Login.js';
 import Payment from './pages/payment/Payment.js';
 
@@ -11,9 +11,9 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/frame" element={<Frame />} />
-        <Route path="/frame-staff" element={<Frame_Staff/>} />
-        <Route path="*" element={<Navigate to="/login"/>} />
-        <Route path="/frame-staff/payment" element={<Payment/>} />
+        <Route path="/frame-staff" element={<FrameStaff />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+        <Route path="/frame-staff/payment" element={<Payment />} />
       </Routes>
     </Router>
   );
